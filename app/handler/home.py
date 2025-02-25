e616b90f1beadaf50c8cf0d2165d8629a5d346ae
Ronaldo Medeiros <ronaldmedeiiros@gmail.com>
2025-02-25 11:54:47 -0400

    ajuste transactions e env_example

--- app/handler/home.py
+++ app/handler/home.py
@@ -9,6 +9,5 @@ class MainHandler(tornado.web.RequestHandler):
                             <h1>Teste Databuild</h1>
                             <p><a href="/alive">/alive</a></p>
                             <p><a href="/read_tpv">/read_tpv</a></p>
-                            <p><a href="/transactions_by_data">/transactionsbydata</a></p>
                         </body>
                     </html>''')
